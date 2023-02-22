ab61551bbf759ad1512f7f69682e0dc4b583043d
Smorodyan <smorodyan@gmail.com>
2023-02-22 15:51:03 +0300

    The program has been written

--- Program.cs
+++ Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Написать программу, которая из имеющегося массива строк формирует массив из строк, 
+// длинна которых меньше либо равна 3 символа. Первоначальный массив можно ввести 
+// с клавиатуры, либо задать на старте выполнения алгоритма.
+
+
+void newText(string[] text)
+{
+    Console.Write("{ ");
+    for(int i = 0; i < text.Length; i++)
+    {
+        string current = text[i];
+        if(current.Length <= 3 )
+            Console.Write(text[i] + "; ");
+    }
+    Console.Write("}");
+}
+
+string[] text = {"hello", "wod", "world", "2"};
+
+for(int i = 0; i < text.Length; i++)
+ Console.Write(text[i] + "; ");
+
+Console.WriteLine();
+newText(text);
+
+
